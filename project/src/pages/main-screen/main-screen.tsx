80c020f7831df68d900f931b9ea345e976208915
adiStas <adistas91@yandex.ru>
2022-10-27 23:14:40 +0300

    вынес отрисовку карточек в цикл

--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -92,11 +92,7 @@ function MainScreen({placeCardCount}: MainScreenProps): JSX.Element {
                 </ul>
               </form>
               <div className="cities__places-list places__list tabs__content">
-                <RoomCard/>
-                <RoomCard/>
-                <RoomCard/>
-                <RoomCard/>
-                <RoomCard/>
+                {[...Array(placeCardCount).keys()].map((i) => <RoomCard key={i} />)}
               </div>
             </section>
             <div className="cities__right-section">
