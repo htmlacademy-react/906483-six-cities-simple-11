10d5e75df730c531dc54fe68b003bbe59283f21a
adiStas <adistas91@yandex.ru>
2022-10-28 21:24:09 +0300

    вынес лого в отдельный компонент

--- project/src/pages/room-screen/room-screen.tsx
+++ project/src/pages/room-screen/room-screen.tsx
@@ -1,4 +1,5 @@
 import HeaderNav from '../../components/header-nav/header-nav';
+import Logo from '../../components/logo/logo';
 
 function RoomScreen(): JSX.Element {
   return (
@@ -7,9 +8,7 @@ function RoomScreen(): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
+              <Logo />
             </div>
             <HeaderNav />
           </div>
