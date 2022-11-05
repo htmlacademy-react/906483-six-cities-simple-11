88cef8a8073f402d9927c349b6786ab7b3125f7b
adiStas <adistas91@yandex.ru>
2022-11-06 00:22:04 +0300

    добавил утилитарных функций

---
+++ project/src/utils.ts
@@ -0,0 +1,25 @@
+import {MAX_OFFER_IMAGES_AMOUNT, MAX_RATING_VALUE, ONE_HUNDRED_PERCENT} from './const';
+
+const getRandomInteger = (min: number, max: number) => {
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
+
+export const calculateRating = (rating: number): number => {
+  const roundRating = Math.round(rating);
+  return roundRating / MAX_RATING_VALUE * ONE_HUNDRED_PERCENT;
+};
+
+export const getRandomOfferImages = (images: string[]): string[] => {
+  const imagesCopy = images.slice(0);
+  if (imagesCopy.length >= MAX_OFFER_IMAGES_AMOUNT) {
+    const selectedImages: string[] = [];
+    for (let i = 0; i < MAX_OFFER_IMAGES_AMOUNT; i++) {
+      const randomInteger = getRandomInteger(0, imagesCopy.length - 1);
+      selectedImages.push(imagesCopy[randomInteger]);
+      imagesCopy.splice(randomInteger, 1);
+    }
+    return selectedImages;
+  }
+  return images;
+};
