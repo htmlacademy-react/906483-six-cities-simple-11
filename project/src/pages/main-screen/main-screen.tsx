2912da357a5ee11499bfd1430503ebe556a0b4b6
adiStas <adistas91@yandex.ru>
2022-11-17 00:18:16 +0300

    вынес навигацию по городам в отдельный компонент

--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -4,6 +4,7 @@ import OfferList from '../../components/offer-list/offer-list';
 import Map from '../../components/map/map';
 import {useState} from 'react';
 import {City} from '../../types/city';
+import LocationList from '../../components/location-list/location-list';
 
 type MainScreenProps = {
   offers: Offers;
@@ -36,38 +37,7 @@ function MainScreen({offers}: MainScreenProps): JSX.Element {
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
-            <ul className="locations__list tabs__list">
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Paris</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Cologne</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Brussels</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item tabs__item--active">
-                  <span>Amsterdam</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Hamburg</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Dusseldorf</span>
-                </a>
-              </li>
-            </ul>
+            <LocationList />
           </section>
         </div>
         <div className="cities">
