f2c738478e57a5ea082cc7801dd616a4075f1fe9
adiStas <adistas91@yandex.ru>
2022-12-12 23:01:52 +0300

    добавил стилей

--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -1,3 +1,4 @@
+import cn from 'classnames';
 import {useAppSelector} from '../../hooks';
 import LocationList from '../../components/location-list/location-list';
 import MainPageContent from '../../components/main-page-content/main-page-content';
@@ -9,16 +10,21 @@ function MainScreen(): JSX.Element {
   const offers = useAppSelector(getOffers);
   return (
     <>
-      <Header />
-      <main className="page__main page__main--index">
+      <Header/>
+      <main className={cn(
+        'page__main',
+        'page__main--index',
+        {'page__main--index-empty': !offers.length}
+      )}
+      >
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
-            <LocationList />
+            <LocationList/>
           </section>
         </div>
         <div className="cities">
-          {offers.length ? <MainPageContent /> : <MainPageEmpty />}
+          {offers.length ? <MainPageContent/> : <MainPageEmpty/>}
         </div>
       </main>
     </>
